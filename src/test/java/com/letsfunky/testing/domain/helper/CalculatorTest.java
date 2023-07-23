43751920de11d59dfca6c671c60313926df6581f
YeongMin <gunjasal@gmail.com>
2023-07-23 15:01:46 +0900

    add AAA to CalculatorTest

--- src/test/java/com/letsfunky/testing/domain/helper/CalculatorTest.java
+++ src/test/java/com/letsfunky/testing/domain/helper/CalculatorTest.java
@@ -1,7 +1,16 @@
 package com.letsfunky.testing.domain.helper;
 
+import org.junit.Test;
+
 class CalculatorTest {
 
     // todo: create tests
+    void 테스트_이름을_변경해주세요() {
+        // arrange (given)
 
+        // act (when)
+
+        // assert (then)
+
+    }
 }
